7b2fb7079d9a8fa7b31f745d60abc345f5919a4e
todocambiodev <todocambiodivisa@gmail.com>
2026-01-19 03:28:25 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="velasJaponeas",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        # Lista de dependencias si las tiene, ej: 'requests>=2.25.1'
+    ],
+)
